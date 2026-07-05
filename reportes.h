5cb6f9bb14950c707ea16dca77c0336129c96b19
Joan Gallardo <joan.gallardo@udla.edu.ec>
2026-07-05 02:33:02 -0500

    creacion de funcionescalculos

---
+++ reportes.h
@@ -0,0 +1,12 @@
+#ifndef REPORTES_H
+#define REPORTES_H
+
+#include "canciones.h"
+
+void mostrarDuracion(Cancion canciones[], int cantidad);
+void tiempoTotal(Cancion canciones[], int cantidad);
+void cancionMasLarga(Cancion canciones[], int cantidad);
+void cancionMasCorta(Cancion canciones[], int cantidad);
+
+
+#endif
