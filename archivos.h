53578d4938d2f09957370259f51ea93746380379
Joan Gallardo <joan.gallardo@udla.edu.ec>
2026-07-05 02:45:40 -0500

    creacion de estructura de archivos

---
+++ archivos.h
@@ -0,0 +1,9 @@
+#ifndef ARCHIVOS_H
+#define ARCHIVOS_H
+
+#include "canciones.h"
+
+void cargarArchivo(Cancion canciones[], int *cantidad);
+void guardarArchivo(Cancion canciones[], int cantidad);
+
+#endif
