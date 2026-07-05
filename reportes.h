2add5067706fc64d9e577708a3c278b987a2884e
Federica Barrera <amaya.barrera@udla.edu.ec>
2026-07-05 02:34:37 -0500

    aumento de funciones a la creacion de calculos

--- reportes.h
+++ reportes.h
@@ -7,6 +7,9 @@ void mostrarDuracion(Cancion canciones[], int cantidad);
 void tiempoTotal(Cancion canciones[], int cantidad);
 void cancionMasLarga(Cancion canciones[], int cantidad);
 void cancionMasCorta(Cancion canciones[], int cantidad);
-
+void cantidadPorClasificacion(Cancion canciones[], int cantidad);
+void promedioDuracion(Cancion canciones[], int cantidad);
+void clasificacionMayor(Cancion canciones[], int cantidad);
+void tiempoPorClasificacion(Cancion canciones[], int cantidad);
 
 #endif
