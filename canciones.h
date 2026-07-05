51ad365ca753e0bed0530c88f593c32de4bacf3d
Federica Barrera <amaya.barrera@udla.edu.ec>
2026-07-05 02:09:50 -0500

    creaciones de funciones

--- canciones.h
+++ canciones.h
@@ -17,6 +17,23 @@ typedef struct {
     int duracion; // en segundos
 } Cancion;
 
+// Declaracion de funciones
+void inicializarCanciones(Cancion canciones[], int *cantidad);
+
+void registrarCancion(Cancion canciones[], int *cantidad);
+void listarCanciones(Cancion canciones[], int cantidad);
+void buscarCancion(Cancion canciones[], int cantidad);
+void actualizarCancion(Cancion canciones[], int cantidad);
+void eliminarCancion(Cancion canciones[], int *cantidad);
+
+int validarCodigo(Cancion canciones[], int cantidad, const char *codigo);
+int buscarPorCodigo(Cancion canciones[], int cantidad, const char *codigo);
+
+void mostrarCancion(Cancion cancion);
+void formatearDuracion(int segundos, char *buffer);
+
+int str_igual_ignore_case(const char *s1, const char *s2);
+char *str_buscar_ignore_case(const char *pajar, const char *aguja);
 
 
 #endif
